14aad3ea5850ab45c9b85d2d24f001d3170bbac7
Francesco Gaetano Larocca <94965502+gaetano-7@users.noreply.github.com>
2024-05-30 17:23:01 +0200

    Update environment.ts

--- chatbot-frontend/src/environments/environment.ts
+++ chatbot-frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://127.0.0.1:8000',
-    apiKey: '8A6AK95-BHBM0ZK-M110B25-T8EEW19',
-    usernameAdmin: 'gaetano_admin',
-    passwordAdmin: 'Gaetanolarocca_7'
+    apiUrl: 'your api-url',
+    apiKey: 'your api-key',
+    usernameAdmin: 'your username-admin',
+    passwordAdmin: 'your password-admin'
 };
